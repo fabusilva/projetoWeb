198bad61f0d51ffab062f684b287b3ebe1e035eb
Fábio Silva <fabio.f3k@gmail.com>
2023-06-15 16:57:35 -0300

    não sei o que to fazendo da vida

--- frontend/src/components/HomeComponent/CardComponent.js
+++ frontend/src/components/HomeComponent/CardComponent.js
@@ -1,35 +1,114 @@
-import style from './CardComponet.module.css';
+import React, { useState } from "react";
+import style from "./CardComponet.module.css";
+import axios from "axios";
+
 export default function CardComponet() {
-    return (
-        <>
-            <div className={style.infoHome}>
-                <div className={style.info1}>
-                    <button className={style.bt1}>Buscar Trabalhos</button>
-                    <p className={style.textoInfo}>
-                        Entre em projetos criado<br></br>
-                        por outras pessoas<br></br>e junte-se a eles de forma indepedente.
-                    </p>
-                </div>
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [endereco, setEndereco] = useState('');    
 
-                <div className={style.info2}>
-                    <button>Criar Trabalhos</button>
-                    <p className={style.textoInfo}>
-                        Tenha a iniciativa e ajude de forma idependente.<br></br>
-                        Crie projetos sociais e reuna-se<br></br>
-                        com pessoas<br></br>
-                        que tem os mesmos interesses que os seu
-                    </p>
-                </div>
+    const handleCausa = async (e) => {
+        e.preventDefault();
+        console.log(title, description, endereco);
+        await axios.post(
+          'http://localhost:3000/api/trabalho',({ title:title, description:description, address:endereco })
+        );
+      };
+      
+  const [isOpen, setIsOpen] = useState(false);
 
-                <div className={style.info3}>
-                    <button>Buscar Causas</button>
-                    <p className={style.textoInfo}>
-                        Ajude uma ONG em sua causa. Uma causa de ONG é uma iniciativa<br></br>
-                        que visa resolver ou minimizar <br></br>
-                        um problema social ou ambiental específico.
-                    </p>
-                </div>
+  const openPopup = () => {
+    setIsOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <div className={style.infoHome}>
+        <div className={style.info1}>
+          <button className={style.bt1}>Buscar Trabalhos</button>
+          <p className={style.textoInfo}>
+            Entre em projetos criado<br></br>
+            por outras pessoas<br></br>e junte-se a eles de forma indepedente.
+          </p>
+        </div>
+
+        <div className={style.info2}>
+          <button onClick={openPopup}>Criar Trabalhos</button>
+          <p className={style.textoInfo}>
+            Tenha a iniciativa e ajude de forma idependente.<br></br>
+            Crie projetos sociais e reuna-se<br></br>
+            com pessoas<br></br>
+            que tem os mesmos interesses que os seu
+          </p>
+        </div>
+
+        <div className={style.info3}>
+          <button>Buscar Causas</button>
+          <p className={style.textoInfo}>
+            Ajude uma ONG em sua causa. Uma causa de ONG é uma iniciativa
+            <br></br>
+            que visa resolver ou minimizar <br></br>
+            um problema social ou ambiental específico.
+          </p>
+        </div>
+      </div>
+      <div>
+        {isOpen && (
+          <div className={style.overlay}>
+            <div className={style.popup}>
+              <div className={style.formContainer}>
+                <form>
+                  <div className={style.formGroup}>
+                    <label htmlFor="title">Titulo do trabalho</label>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      placeholder="Nome do trabalho voluntario"
+                      required
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <label htmlFor="description">Descrição</label>
+                    <input
+                      type="text"
+                      id="description"
+                      name="description"
+                      placeholder="Faça uma descrição do trabalho"
+                      required
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <label htmlFor="endereco">Endereço</label>
+                    <input
+                      type="text"
+                      id="endereco"
+                      name="endereco"
+                      placeholder="Endereço do local"
+                      required
+                      onChange={(e) => setEndereco(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <button type="submit" onClick={(e) => handleCausa(e)}>
+                      Criar
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <button className={style.btfechar} onClick={closePopup}>
+                X
+              </button>
             </div>
-        </>
-    )
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
--- frontend/src/pages/Trabalho/Trabalho.js
+++ frontend/src/pages/Trabalho/Trabalho.js
@@ -1,6 +1,135 @@
-import Menu from '../../components/MenuComponent/MenuComponet';
-export default function Trabalho(){
-    return(
-        <Menu/>
-    )
+import React,{ useEffect, useState } from "react";
+
+import Menu from "../../components/MenuComponent/MenuComponet";
+import axios from "axios";
+import style from "./Trabalho.module.css";
+export default function Trabalho() {
+  const [content, receivedContent] = useState([]);
+  const getAll = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/trabalho");
+      const data = response.data;
+      receivedContent(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAll();
+  }, []);
+  //console.log(content)
+  async function deletando(id) {
+    console.log(id);
+    try {
+      await axios.delete(`http://localhost:3000/api/trabalho/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [id,setId] = setId("");
+
+  const handleCausa = async (e) => {
+    e.preventDefault();
+    console.log(title, description, endereco);
+    await axios.put(`http://localhost:3000/api/trabalho/${id}`, {
+      title: title,
+      description: description,
+      address: endereco,
+    });
+  };
+  //popup de formulario +update
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsOpen(true);
+    setTitle("ola")
+    setDescription("olaa")
+    setEndereco("aaaa")
+  };
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <Menu />
+      <div className={style.divConteiner}>
+        <h1>Trabalhos Disponiveis</h1>
+        {content.length === 0 ? (
+          <p>Carregando...</p>
+        ) : (
+          content.map((trabalho) => (
+            <div className="post" key={trabalho._id}>
+              <h3>{trabalho.title}</h3>
+              <p>{trabalho.description}</p>
+              <p>{trabalho.address}</p>
+              <p>{trabalho._id}</p>
+              onChange={(e) => setTitle(trabalho._id)}
+              <button onClick={openPopup}>Update</button>
+              <button onClick={() => deletando(trabalho._id)}>Delete</button>
+            </div>
+          ))
+        )}
+      </div>
+      <div>
+        {isOpen && (
+          <div className={style.overlay}>
+            <div className={style.popup}>
+              <div className={style.formContainer}>
+                <form>
+                  <div className={style.formGroup}>
+                    <label htmlFor="title">Titulo do trabalho</label>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      placeholder="Nome do trabalho voluntario"
+                      required
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <label htmlFor="description">Descrição</label>
+                    <input
+                      type="text"
+                      id="description"
+                      name="description"
+                      placeholder="Faça uma descrição do trabalho"
+                      required
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <label htmlFor="endereco">Endereço</label>
+                    <input
+                      type="text"
+                      id="endereco"
+                      name="endereco"
+                      placeholder="Endereço do local"
+                      required
+                      onChange={(e) => setEndereco(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.formGroup}>
+                    <button type="submit" onClick={(e) => handleCausa(e)}>
+                      Criar
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <button className={style.btfechar} onClick={closePopup}>
+                X
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+
+    </>
+  );
 }
